5daad684b6d6b1823234c84e1bf66a86f7bee966
stigok <stig@stigok.com>
2018-01-24 16:14:04 +0100

    Configure with environment variables

--- server.js
+++ server.js
@@ -1,34 +1,46 @@
-const IRCClient = require('./irc-client')
+const assert = require('assert')
 const express = require('express')
 const bodyParser = require('body-parser')
+const IRCClient = require('./irc-client')
 
 const HTTP_PORT = process.env.HTTP_PORT || 3000
+const IRC_HOST = process.env.IRC_HOST
+const IRC_PORT = process.env.IRC_PORT || 6667
+const IRC_NICK = process.env.IRC_NICK
+const IRC_CHANNEL = process.env.IRC_CHANNEL
 
-const chan = '#hackeriet'
+// Quick sanity check
+assert(IRC_HOST, 'Missing IRC_HOST')
+assert(IRC_PORT, 'Missing IRC_PORT')
+assert(IRC_NICK, 'Missing IRC_NICK')
+assert(IRC_CHANNEL, 'Missing IRC_CHANNEL')
+
 const app = express()
+
+// To hold the server instance later on
 let server
 
 // IRC Client
 console.log('Connecting to IRC')
 const client = new IRCClient({
-  host: 'chat.freenode.net',
-  port: 6667,
-  nick: 'sshowfojs'
+  host: IRC_HOST,
+  port: IRC_PORT,
+  nick: IRC_NICK
 })
 
 // HTTP API
-app.post('/hackeriet', bodyParser.json(), (req, res, next) => {
+app.post(`/${IRC_CHANNEL}`, bodyParser.json(), (req, res, next) => {
   const msg = req.body.msg
 
   if (!msg || !msg.length)
     return res.status(400).json({error: 'Message must be longer than 0 chars'})
 
-  //client.msg(chan, msg)
-  client.notice(chan, msg)
+  //client.msg(IRC_CHANNEL, msg)
+  client.notice(IRC_CHANNEL, msg)
   return res.status(201).json({error: null})
 })
 
-client.on('ready', () => client.join(chan))
+client.on('ready', () => client.join(`#IRC_CHANNEL`))
 client.on('end', () => {
   process.exit(0)
   // Close HTTP server too
